f1a472831c1da84c2ffdbd0bdabba7e405b0a300
Giovani <gscaduto@sabesp.com.br>
2020-01-28 15:05:58 -0300

    First Commit

---
+++ QuickSort/Carro.cs
@@ -0,0 +1,19 @@
+﻿namespace QuickSort
+{
+    public class Carro
+    {
+        public Carro(string nome, decimal preco)
+        {
+            Nome = nome;
+            Preco = preco;
+        }
+
+        public string Nome { get; set; }
+        public decimal Preco { get; set; }
+
+        public override string ToString()
+        {
+            return $"Carro: {Nome}, Valor: R$ {Preco.ToString("N2")}";
+        }
+    }
+}
---
+++ QuickSort/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace QuickSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Carro[] carros =
+            {
+                new Carro("Ferrari", 500_000),
+                new Carro("Hilux", 150_000),
+                new Carro("Gol", 40_000),
+                new Carro("Onix", 40_000),
+                new Carro("Mobi", 30_000),
+                new Carro("Strada", 45_000),
+                new Carro("EcoSport", 71_000)
+            };
+
+            Ordena(carros, 0, carros.Length);
+
+            ImprimeCarros(carros, "Carros Ordenados");
+
+            Console.ReadKey();
+        }
+
+        private static void Ordena(Carro[] carros, int de, int ate)
+        {
+            int elementos = ate - de;
+            int posicaoPivo;
+
+            if (elementos > 1)
+            {
+                posicaoPivo = Particiona(carros, de, ate);
+                Ordena(carros, de, posicaoPivo);
+                Ordena(carros, posicaoPivo + 1, ate);
+            }
+        }
+
+        private static int Particiona(Carro[] carros, int inicio, int termino)
+        {
+            int menoresEncontrados = inicio;
+
+            Carro pivo = carros[termino - 1];
+            Carro atual;
+
+            for (int analisando = inicio; analisando < termino - 1; analisando++)
+            {
+                atual = carros[analisando];
+
+                if(atual.Preco <= pivo.Preco)
+                {
+                    Troca(carros, analisando, menoresEncontrados);
+                    menoresEncontrados++;
+                }
+            }
+            
+            Troca(carros, termino - 1, menoresEncontrados);
+            
+            return menoresEncontrados;
+        }
+
+        private static void Troca(Carro[] carros, int de, int para)
+        {
+            Carro carro1 = carros[de];
+            Carro carro2 = carros[para];
+
+            carros[para] = carro1;
+            carros[de] = carro2;
+        }
+
+        private static void ImprimeCarros(Carro[] carros, string titulo)
+        {
+            Console.WriteLine(titulo);
+            Console.WriteLine();
+            foreach (var carro in carros)
+            {
+                Console.WriteLine(carro.ToString());
+            }
+            Console.WriteLine();
+        }
+    }
+}
